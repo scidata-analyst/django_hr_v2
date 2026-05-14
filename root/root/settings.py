c0f72c22a4a9e2f3b742cdfaa416ca26f12c870e
kanchonkumar <kanchonkumar49@gmail.com>
2026-05-14 23:48:53 +0600

    port changed

--- root/root/settings.py
+++ root/root/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-rah$27@*u4&@pr%v2x87p@%5fo_q+y*l=@ymav%$lwevhx3rkw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['51.20.190.115', 'localhost', '127.0.0.1']
 
 # Application definition
 
