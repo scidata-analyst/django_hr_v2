82cbf11ddc9d0480686b33658ba106e280a03598
kanchonkumar <kanchonkumar49@gmail.com>
2026-08-07 18:31:37 +0600

    pagination issue fixed

--- root/main/static/main/js/APIClient.js
+++ root/main/static/main/js/APIClient.js
@@ -100,14 +100,15 @@ class APIClient {
      * @param {string|Function} endpoint - API endpoint
      * @param {Object} queryParams - Query parameters
      * @param {string} [queryParams.search=''] - Search keyword
+     * @param {number} [queryParams.page=1] - Current page
      * @param {number} [queryParams.page_size=15] - Items per page
      * @param {string} [queryParams.sort_by='id'] - Sort column
      * @param {string} [queryParams.sort_direction='desc'] - Sort direction
      * @returns {Promise<Object>} Paginated records
      */
     async index(endpoint, queryParams = {}) {
-        const { search = '', page_size = 15, sort_by = 'id', sort_direction = 'desc' } = queryParams;
-        const query = new URLSearchParams({ search, page_size, sort_by, sort_direction }).toString();
+        const { search = '', page = 1, page_size = 15, sort_by = 'id', sort_direction = 'desc' } = queryParams;
+        const query = new URLSearchParams({ search, page, page_size, sort_by, sort_direction }).toString();
         const urlPath = this._getEndpoint(endpoint);
         return this._request('GET', `${urlPath}?${query}`);
     }
